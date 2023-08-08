851c0a58243a35528b374ac0cfafe5b34e5c4ac4
mk812y <mk812y@gmail.com>
2023-08-08 18:49:59 +0200

    ok

--- TimerInSwiftUI/ContentView.swift
+++ TimerInSwiftUI/ContentView.swift
@@ -8,12 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var currentDate = Date.now
+    @State var count = 0
+    
+    @State var dateTimer = Timer.publish(every: 1, on: .main, in: .common).autoconnect() //date timer update the time for every second
+    @State var countTimer = Timer.publish(every: 1, on: .main, in: .common).autoconnect() //update the counter
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Date: \(currentDate.formatted(date: .numeric, time: .standard))")
+                .font(.title)
+                .onReceive(dateTimer) { input in
+                    currentDate = input
+                }
+            Text("Count: \(count)")
+                .font(.largeTitle)
+                .onReceive(countTimer) { _ in
+                    count += 1
+                }
+            Group {
+                Button("Stop the timers") {
+                    dateTimer.upstream.connect().cancel()
+                    countTimer.upstream.connect().cancel()
+                }
+                Button("Restart the timer") {
+                    dateTimer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+                    countTimer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+                }
+                .padding()
+                .background(.blue)
+                .cornerRadius(8)
+                .foregroundColor(.white)
+            }
         }
         .padding()
     }
