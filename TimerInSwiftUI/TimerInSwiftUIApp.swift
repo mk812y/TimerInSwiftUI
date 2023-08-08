8683a61d8da3aab7f6f38116767f6ce6a5a9e14f
mk812y <mk812y@gmail.com>
2023-08-08 18:37:09 +0200

    Initial Commit

---
+++ TimerInSwiftUI/TimerInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerInSwiftUIApp.swift
+//  TimerInSwiftUI
+//
+//  Created by Михаил Куприянов on 8.8.23..
+//
+
+import SwiftUI
+
+@main
+struct TimerInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
